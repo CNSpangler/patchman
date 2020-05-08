2f87e6cfc9e4278c4dffd1ae604a690370d1118c
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-07 18:05:43 -0700

    add custom hooks

--- src/hooks/StateProvider.jsx
+++ src/hooks/StateProvider.jsx
@@ -1,8 +1,10 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeRequest } from '../../services/apiCall';
 
-const StateProvider = ({ children }) => {
+const StateContext = createContext();
+
+export const StateProvider = ({ children }) => {
   const [url, setUrl] = useState('');
   const [body, setBody] = useState('');
   const [method, setMethod] = useState('GET');
@@ -49,4 +51,47 @@ StateProvider.propTypes = {
   children: PropTypes.node
 };
 
-export default StateProvider;
+export const useUrl = () => {
+  const { url } = useContext(StateContext);
+  return url;
+};
+
+export const useBody = () => {
+  const { body } = useContext(StateContext);
+  return body;
+};
+
+export const useMethod = () => {
+  const { method } = useContext(StateContext);
+  return method;
+};
+
+export const useResults = () => {
+  const { results } = useContext(StateContext);
+  return results;
+};
+
+export const useHistory = () => {
+  const { history } = useContext(StateContext);
+  return history;
+};
+
+export const useHandleUrlChange = () => {
+  const { handleUrlChange } = useContext(StateContext);
+  return handleUrlChange;
+};
+
+export const useHandleMethodChange = () => {
+  const { handleMethodChange } = useContext(StateContext);
+  return handleMethodChange;
+};
+
+export const useHandleBodyChange = () => {
+  const { handleBodyChange } = useContext(StateContext);
+  return handleBodyChange;
+};
+
+export const useHandleSubmit = () => {
+  const { handleSubmit } = useContext(StateContext);
+  return handleSubmit;
+};
